a6f55475cb958088b22319e95b2e115b1403d6a6
Gleb Lipay <gleb.lipai@yandex.ru>
2023-06-12 13:43:26 +0300

    Generating random data for tests with Faker

---
+++ src/test/java/com/demoqa/tests/RegistrationWithFakerTests.java
@@ -0,0 +1,59 @@
+package com.demoqa.tests;
+
+import com.demoqa.pages.RegistrationPage;
+import com.demoqa.utils.GetRandomData;
+import org.junit.jupiter.api.Test;
+
+public class RegistrationWithFakerTests extends BaseTest{
+
+    RegistrationPage registrationPage = new RegistrationPage();
+    GetRandomData randomData = new GetRandomData();
+
+    String firstName = randomData.getFirstName(),
+           lastName = randomData.getLastName(),
+           email = randomData.getEmail(),
+           gender = randomData.getGender(),
+           phoneNumber = randomData.getPhoneNumber(),
+           day = randomData.getDay(),
+           month = randomData.getMonth(),
+           year = randomData.getYear(),
+           subject = randomData.getSubjects(),
+           hobby = randomData.getHobbies(),
+           fileName = "1.png",
+           address = randomData.getAddress(),
+           state = randomData.getState(),
+           city = randomData.getCity(state);
+
+
+    @Test
+    void successfulRegistrationTest() {
+
+        registrationPage.openPage().executeJs()
+                .setFirstName(firstName)
+                .setLastName(lastName)
+                .setUserEmail(email)
+                .setGender(gender)
+                .setUserNumber(phoneNumber)
+                .setBirthDay(day, month, year)
+                .setSubject(subject)
+                .setHobbies(hobby)
+                .uploadFile(fileName)
+                .setAddress(address)
+                .setState(state)
+                .setCity(city)
+                .clickSubmit();
+
+
+        registrationPage
+                .checkResult("Student Name", firstName + " " + lastName)
+                .checkResult("Student Email", email)
+                .checkResult("Gender", gender)
+                .checkResult("Mobile", phoneNumber)
+                .checkResult("Date of Birth", day + " " + month + "," + year)
+                .checkResult("Subjects", subject)
+                .checkResult("Hobbies", hobby)
+                .checkResult("Picture", "1.png")
+                .checkResult("Address", address)
+                .checkResult("State and City", state + " " + city);
+    }
+}
---
+++ src/test/java/com/demoqa/utils/GetRandomData.java
@@ -0,0 +1,89 @@
+package com.demoqa.utils;
+
+import com.github.javafaker.Faker;
+
+public class GetRandomData {
+
+    Faker faker = new Faker();
+
+    public String getFirstName() {
+        return faker.name().firstName();
+    }
+
+    public String getLastName() {
+        return faker.name().lastName();
+    }
+
+    public String getEmail() {
+        return faker.internet().emailAddress();
+    }
+
+    public String getGender() {
+        String[] genders = {"Male", "Female", "Other"};
+        return faker.options().option(genders);
+    }
+
+    public String getPhoneNumber() {
+        return faker.number().digits(10);
+    }
+
+    public String getDay() {
+        return String.format("%02d", faker.number().numberBetween(1, 28));
+    }
+
+    public String getMonth() {
+        String[] months = {"January", "February", "March", "April", "May",
+                "June", "July", "August", "September", "October", "November",
+                "December"};
+        return faker.options().option(months);
+    }
+
+    public String getYear() {
+        return String.valueOf(faker.number().numberBetween(1900, 2100));
+    }
+
+    public String getSubjects() {
+        String[] subjects = {"Math", "English", "Chemistry", "Civics",
+                "Computer Science", "Arts", "Physics", "Economics"};
+        return faker.options().option(subjects);
+    }
+
+    public String getHobbies() {
+        String[] hobbies = {"Sports", "Reading", "Music"};
+        return faker.options().option(hobbies);
+    }
+
+    public String getAddress() {
+        return faker.address().fullAddress();
+    }
+
+    public String getState() {
+        String[] state = {"NCR", "Uttar Pradesh", "Haryana", "Rajasthan"};
+        return faker.options().option(state);
+    }
+
+    public String getCity(String state) {
+        switch (state) {
+            case "NCR": {
+                String[] city = {"Delhi", "Gurgaon", "Noida"};
+                return faker.options().option(city);
+            }
+            case "Uttar Pradesh": {
+                String[] city = {"Agra", "Lucknow", "Merrut"};
+                return faker.options().option(city);
+            }
+            case "Haryana": {
+                String[] city = {"Karnal", "Panipat"};
+                return faker.options().option(city);
+            }
+            case "Rajasthan": {
+                String[] city = {"Jaipur", "Jaiselmer"};
+                return faker.options().option(city);
+            }
+        }
+        return null;
+    }
+
+
+
+}
