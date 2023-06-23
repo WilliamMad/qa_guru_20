d969d0f4eb182bad23dbea98edbf66db346ddf78
Gleb Lipay <gleb.lipai@yandex.ru>
2023-06-24 00:08:17 +0300

    Вынесение классов ProductProviders и Provider в отдельный блок модулей

---
+++ src/test/java/qaguru/hw10/models/ProductProviders.java
@@ -0,0 +1,17 @@
+package qaguru.hw10.models;
+
+import java.util.List;
+
+public class ProductProviders {
+    private String productId;
+    private List<Provider> providers;
+
+    public String getProductId() {
+        return productId;
+    }
+    public List<Provider> getProviders() {
+        return providers;
+    }
+}
+
+
---
+++ src/test/java/qaguru/hw10/models/Provider.java
@@ -0,0 +1,19 @@
+package qaguru.hw10.models;
+
+public class Provider {
+    private String name;
+    private String contact;
+    private String email;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
