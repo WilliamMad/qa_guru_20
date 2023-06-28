94df2bf75eb481803d251c6166620569be0ef6ba
Gleb Lipay <gleb.lipai@yandex.ru>
2023-06-28 15:13:02 +0300

    Remote setup + allure

---
+++ src/test/java/qaguru/demoqa/tests/BaseTestRemote.java
@@ -0,0 +1,51 @@
+package qaguru.demoqa.tests;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Map;
+
+import static com.codeborne.selenide.Configuration.*;
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+import static qaguru.demoqa.helpers.Attachments.*;
+
+
+public class BaseTestRemote {
+
+    @BeforeAll
+     void beforeAll() {
+        baseUrl = "https://demoqa.com";
+        browserSize = "1920x1080";
+        holdBrowserOpen = true;
+        pageLoadStrategy = "eager";
+
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+
+        Configuration.browserCapabilities = capabilities;
+    }
+
+    @BeforeEach
+    void addListener() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+    }
+
+    @AfterEach
+    void addAttachments() {
+        screenshotAs("Last screenshot");
+        pageSource();
+        browserConsoleLogs();
+        addVideo();
+
+        closeWebDriver();
+    }
+}
---
+++ src/test/java/qaguru/demoqa/tests/HW12/RegistrationWithAllureTests.java
@@ -0,0 +1,63 @@
+package qaguru.demoqa.tests.HW12;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import qaguru.demoqa.pages.RegistrationPage;
+import qaguru.demoqa.tests.BaseTestRemote;
+import qaguru.demoqa.utils.GetRandomData;
+
+public class RegistrationWithAllureTests extends BaseTestRemote {
+
+    RegistrationPage registrationPage = new RegistrationPage();
+    GetRandomData randomData = new GetRandomData();
+
+    String firstName = randomData.getFirstName(),
+           lastName = randomData.getLastName(),
+           email = randomData.getEmail(),
+           gender = randomData.getGender(),
+           phoneNumber = randomData.getPhoneNumber(),
+           day = randomData.getDay(),
+           month = randomData.getMonth(),
+           year = randomData.getYear(),
+           subject = randomData.getSubjects(),
+           hobby = randomData.getHobbies(),
+           fileName = "1.png",
+           address = randomData.getAddress(),
+           state = randomData.getState(),
+           city = randomData.getCity(state);
+
+
+    @Test
+    @Tag("remote")
+    void successfulRegistrationTest() {
+
+        registrationPage.openPage().executeJs()
+                .setFirstName(firstName)
+                .setLastName(lastName)
+                .setUserEmail(email)
+                .setGender(gender)
+                .setUserNumber(phoneNumber)
+                .setBirthDay(day, month, year)
+                .setSubject(subject)
+                .setHobbies(hobby)
+                .uploadFile(fileName)
+                .setAddress(address)
+                .setState(state)
+                .setCity(city)
+                .clickSubmit();
+
+
+        registrationPage
+                .checkResult("Student Name", firstName + " " + lastName)
+                .checkResult("Student Email", email)
+                .checkResult("Gender", gender)
+                .checkResult("Mobile", phoneNumber)
+                .checkResult("Date of Birth", day + " " + month + "," + year)
+                .checkResult("Subjects", subject)
+                .checkResult("Hobbies", hobby)
+                .checkResult("Picture", fileName)
+                .checkResult("Picture", "1.png")
+                .checkResult("Address", address)
+                .checkResult("State and City", state + " " + city);
+    }
+}
