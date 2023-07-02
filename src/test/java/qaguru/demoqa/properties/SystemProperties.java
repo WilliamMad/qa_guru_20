036da62125eb2eca4b8d7929242724aa82c6110a
Gleb Lipay <gleb.lipai@yandex.ru>
2023-07-02 15:47:09 +0300

    adding system properties

--- src/test/java/qaguru/demoqa/properties/SystemProperties.java
+++ src/test/java/qaguru/demoqa/properties/SystemProperties.java
@@ -1,21 +1,21 @@
 package qaguru.demoqa.properties;
 
-import org.junit.jupiter.api.Test;
-
 public class SystemProperties {
 
-    @Test
     void setProperties() {
         System.setProperty("browser", "chrome");
         String browser = System.getProperty("browser", "safari");
-        System.out.println(browser);
 
         System.setProperty("version", "114");
         String version = System.getProperty("version", "101");
-        System.out.println(version);
 
         System.setProperty("windowSize", "1920x1080");
         String windowSize = System.getProperty("windowSize", "1366x768");
-        System.out.println(windowSize);
+
+        System.setProperty("baseUrl", "https://demoqa.com");
+        String baseUrl = System.getProperty("baseUrl", "https://demoqa.com");
+
+        System.setProperty("remoteUrl", "https://user1:1234@selenoid.autotests.cloud/wd/hub");
+        String url = System.getProperty("remoteUrl", "https://user1:1234@selenoid.autotests.cloud/wd/hub");
     }
 }
